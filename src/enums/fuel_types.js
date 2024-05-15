5c0a552e53815bd2207032e98779bbda829de08a
Nikolaos Ktistakis <nktistakis@crowdpolicy.com>
2024-05-15 16:24:55 +0300

    added build

--- src/enums/fuel_types.js
+++
@@ -1,7 +0,0 @@
-const FUEL_TYPES = {
-  petrol: "petrol",
-  diesel: "diesel",
-  electric: "electric",
-};
-
-module.exports = FUEL_TYPES;
--- src/enums/gear_types.js
+++
@@ -1,6 +0,0 @@
-const GEAR_TYPES = {
-  manual: "manual",
-  automatic: "automatic",
-};
-
-module.exports = GEAR_TYPES;
